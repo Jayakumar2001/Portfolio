051926c8934e96e3f36487f2164e70f6bf295f0c
Jayakumar2001 <jayakumar242001@gmail.com>
2024-03-02 19:17:31 +0530

    intalize

---
+++ src/data/constants.js
@@ -0,0 +1,151 @@
+export const Bio = {
+  name: "Jayakumar",
+  roles: [
+    "React js Developer"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Jayakumar2001",
+  resume:
+    "https://drive.google.com/file/d/1EaXVVN__SQz_bim3j708_v4ALyhcrhxx/view?usp=drive_link",
+  twitter: "https://twitter.com/jayakumar242001",
+  linkedin :"https://www.linkedin.com/in/jayakumar-v-9b6b25262/",
+  insta: "https://www.instagram.com/jaya_kumar2001/",
+  facebook: "https://www.facebook.com/profile.php?id=100042918814954",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "React developer",
+    company: "FITA Academy",
+    date: "Sep 2023 - Jan 2024",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+      }
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "St.Joseph College of Engineering, Sriperumbudur",
+    date: "Jun 2019 - Apr 2023",
+    grade: "8.28 CGPA",
+    desc: "I'm a motivated B.E. graduate from St. Joseph College of Engineering with a CGPA of 8.24. Passionate about technology, I thrive on turning ideas into impactful solutions. Eager to contribute to innovative projects and make a difference in the world.",
+    degree: "Bachelor of Engineering - B.E, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    school: "Maxwell Matriculation HR.SEC.School, Thanjavur",
+    date: "2019",
+    grade: "60%",
+    desc: "I completed my class 12 high school education at Maxwell Matriculation HR.SEC.School,Thanjavur. where I studied  Computer Science.",
+    degree: "Computer science",
+  },
+  {
+    id: 2,
+    school: "Maxwell Matriculation HR.SEC.School, Thanjavur",
+    date: "2017",
+    grade: "74%",
+    desc: "I completed my class 10 education at Maxwell Matriculation HR.SEC.School,Thanjavur, where I studied  Computer science.",
+    degree: " Computer Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Todo Web App",
+    date: "Jan 2024",
+    description:
+      " A Todo Web App made with React JS, Redux. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+    image:
+      "https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    tags: ["React Js", "Local Storage"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+];
